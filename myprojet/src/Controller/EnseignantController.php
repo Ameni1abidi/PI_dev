376b39ac9f836329f57f9e31469295e1b3d95559
Ameni Abidi <amenizina12@gmail.com>
2026-02-06 18:09:35 +0100

    evaluation

---
+++ myprojet/src/Controller/EnseignantController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EnseignantController extends AbstractController
+{
+    #[Route('/enseignant/dashboard', name: 'app_enseignant_dashboard', methods: ['GET'])]
+    public function dashboard(): Response
+    {
+        return $this->render('enseignant/dashboard.html.twig');
+    }
+}
