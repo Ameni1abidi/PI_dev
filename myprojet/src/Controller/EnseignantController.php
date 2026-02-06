55b9830a07eaf26c29e4697f35322f939ca272f0
Ameni Abidi <amenizina12@gmail.com>
2026-02-07 00:06:10 +0100

    change examen

--- myprojet/src/Controller/EnseignantController.php
+++ myprojet/src/Controller/EnseignantController.php
@@ -13,4 +13,10 @@ final class EnseignantController extends AbstractController
     {
         return $this->render('enseignant/dashboard.html.twig');
     }
+
+    #[Route('/enseignant/evaluations', name: 'app_enseignant_evaluations', methods: ['GET'])]
+    public function evaluations(): Response
+    {
+        return $this->render('enseignant/evaluations.html.twig');
+    }
 }
