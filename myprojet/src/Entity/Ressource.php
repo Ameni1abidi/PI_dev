034ec7799bfd60acddd63f432e25c2c22b6796ec
mariemrahmouni <Mariem.Rahmouni@esprit.tn>
2026-02-09 21:03:16 +0100

    Mes changements locaux

--- myprojet/src/Entity/Ressource.php
+++ myprojet/src/Entity/Ressource.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\RessourceRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\Categorie;
 
 #[ORM\Entity(repositoryClass: RessourceRepository::class)]
 class Ressource
@@ -14,18 +16,42 @@ class Ressource
     #[ORM\Column]
     private ?int $id = null;
 
+    // Titre obligatoire, longueur min 3 max 150
     #[ORM\Column(length: 150)]
+    #[Assert\NotBlank(message: "Le titre est obligatoire")]
+    #[Assert\Length(
+        min: 3,
+        max: 150,
+        minMessage: "Le titre doit contenir au moins {{ limit }} caractères",
+        maxMessage: "Le titre ne doit pas dépasser {{ limit }} caractères"
+    )]
     private ?string $titre = null;
 
+    // Type obligatoire, choix limité
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(message: "Le type est obligatoire")]
+    #[Assert\Choice(
+        choices: ["PDF", "VIDEO", "ARTICLE"],
+        message: "Le type doit être PDF, VIDEO ou ARTICLE"
+    )]
     private ?string $type = null;
 
+    // Contenu obligatoire, longueur min 10
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: "Le contenu est obligatoire")]
+    #[Assert\Length(
+        min: 10,
+        minMessage: "Le contenu doit contenir au moins {{ limit }} caractères"
+    )]
     private ?string $contenu = null;
 
+    // Catégorie obligatoire
     #[ORM\ManyToOne(inversedBy: 'ressources')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?categorie $categorie = null;
+    #[Assert\NotNull(message: "La catégorie est obligatoire")]
+    private ?Categorie $categorie = null;
+
+    // -------------------- GETTERS & SETTERS --------------------
 
     public function getId(): ?int
     {
@@ -40,7 +66,6 @@ class Ressource
     public function setTitre(string $titre): static
     {
         $this->titre = $titre;
-
         return $this;
     }
 
@@ -52,7 +77,6 @@ class Ressource
     public function setType(string $type): static
     {
         $this->type = $type;
-
         return $this;
     }
 
@@ -64,19 +88,17 @@ class Ressource
     public function setContenu(string $contenu): static
     {
         $this->contenu = $contenu;
-
         return $this;
     }
 
-    public function getCategorie(): ?categorie
+    public function getCategorie(): ?Categorie
     {
         return $this->categorie;
     }
 
-    public function setCategorie(?categorie $categorie): static
+    public function setCategorie(?Categorie $categorie): static
     {
         $this->categorie = $categorie;
-
         return $this;
     }
 }
